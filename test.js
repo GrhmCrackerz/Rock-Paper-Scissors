9e30927a55952941a7a995145203aae05018ff4b
Rodney Graham <88797817+GrhmCrackerz@users.noreply.github.com>
2024-01-13 15:13:16 -0500

    Created game method.

--- test.js
+++ test.js
@@ -11,8 +11,6 @@ function computerPicks(){
     return computerChoices[randomChoice]
 }
 
-console.log(computerPicks)
-
 function playRound(playerSelection, computerSelection){
     if(playerSelection === computerSelection){
         return "It's a tie!"
@@ -25,7 +23,45 @@ function playRound(playerSelection, computerSelection){
     return "The computer wins!"
 }
 
-const playerSelection = "scissors"
+/*
+const playerSelection = prompt("Choose rock, paper, or scissors:")
+
 const computerSelection = computerPicks()
 const result = playRound(playerSelection, computerSelection)
-console.log(`${result} (${playerSelection} vs ${computerSelection})`)
+console.log(`${result} (${playerSelection} beats ${computerSelection})`)
+*/
+
+function game(){
+    // This function is going to reflect a game of best of 5
+    let playerScore = 0;
+    let computerScore = 0;
+    let roundResult, playerSelection, computerSelection;
+  
+    for(let round = 1; round <= 5; round++){
+        playerSelection = prompt("Choose rock, paper, or scissors:");
+        computerSelection = computerPicks();
+        roundResult = playRound(playerSelection, computerSelection);
+
+        console.log(`Round ${round}: ${roundResult} (Player chose ${playerSelection}, Computer chose ${computerSelection})`);
+
+        if (roundResult.includes("You win")){
+            playerScore++;
+        } else if (roundResult.includes("The computer wins")){
+            computerScore++;
+        } else{
+            //If its a tie, we reduce the round to still meet the best out of 5 condition
+            round--;
+        }
+        
+    }
+
+    if(playerScore > computerScore){
+        console.log(`You won the game! Final score: Player ${playerScore}, Computer ${computerScore}`);
+    } else if(computerScore > playerScore){
+        console.log(`Computer won the game! Final score: Player ${playerScore}, Computer ${computerScore}`);
+    } else {
+        console.log(`It's a tie! Replay the round! Final score: Player ${playerScore}, Computer ${computerScore}`);
+    }
+}
+
+game();
